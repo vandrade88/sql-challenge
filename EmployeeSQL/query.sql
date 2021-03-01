86218a83dcaf25d98a769f31eeadc6c69c758561
Valerie <vandrade07@yahoo.com>
2021-03-01 18:48:21 -0500

    updated file

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -39,7 +39,8 @@ ON departments.dept_no = dept_emp.dept_no;
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex
 FROM employees
-WHERE
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
 
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
